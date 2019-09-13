6ba02ded2a2c8625cdc06eafbb1928e788bc44a0
socgend03@iiht.tech <socgend03@iiht.tech>
2019-09-13 10:21:08 +0530

    first commit

---
+++ RecruitmentDB/recruitmentmvc/Recruitment_Application/Controllers/HiringManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recruitment_Application.Controllers
+{
+    public class HiringManagerController : Controller
+    {
+        // GET: HiringManager
+        public ActionResult ReqAllotment()
+        {
+            return View();
+        }
+    }
+}
